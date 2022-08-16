eab5ed475726f749273fbff8173c01a3a54c0825
Caroline Lima <carolaite@gmail.com>
2022-08-16 19:18:13 -0300

    feat: added mão na massa templates and exercises

--- mao-na-massa-templates/modulo-4/aula-2/index.js
+++ mao-na-massa-templates/modulo-4/aula-2/index.js
@@ -4,8 +4,22 @@ form.addEventListener("submit", (event) => {
   event.preventDefault()
 
   console.log({
+    name: form.name.value,
     email: form.email.value,
-    password: form.password.value,
-    remeberMe: form.rememberMe.checked,
+    whatsapp: form.whatsapp.value,
+    gender: form.gender.value,
+    description: form.description.value,
+    skillLevel: form.skillLevel.value,
+    terms: form.terms.checked,
   })
 })
+
+function clearData() {
+  form.name.value = ""
+  form.email.value = ""
+  form.whatsapp.value = ""
+  form.gender.value = "male"
+  form.description.value = ""
+  form.skillLevel.value = ""
+  form.terms.checked = false
+}
